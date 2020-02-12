c8ee301e34560eae51a5acb032bd699802ea4e03
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-02-11 21:17:51 -0300

    conceito de class components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import profile from './assets/Avatar Ícaro.jpeg'
+import TechList from './components/TechList'
+
 
 function App() {
-  return <img width="200" src={profile}></img>
+  return <TechList/>
 }
 
 export default App;
