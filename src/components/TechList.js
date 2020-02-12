424d334d7bd76b3cc26250ffb5c6ab702e052ef3
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-02-11 21:32:37 -0300

    conceito de estado e imutabilidade

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 export default class TechList extends Component {
 
   state = {
+    newTech: '',
     techs: [
       'Node.js',
       'ReactJS',
@@ -11,13 +12,31 @@ export default class TechList extends Component {
     ]
   };
 
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({ techs: [...this.state.techs, this.state.newTech],
+    newTech: ''
+    });
+  }
+
   render() {
     return (
+      <form onSubmit={this.handleSubmit}>
       <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
+        {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
       </ul>
+      <input 
+      type="text" 
+      onChange={this.handleInputChange}
+      value={this.state.newTech}
+      />
+      <button type="submit">Enviar</button>
+      </form>
     )
   }
 }
